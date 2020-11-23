34455d96269a17567250f1a6dcfeab8349ae8423
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-11-23 11:47:42 +0100

    exo50

---
+++ exo 1/add.js
@@ -0,0 +1,8 @@
+let name = "Nom : coquelet";
+let prenom = "Prenom :samuel";
+let ville = "Ville :fourmies";
+let bonjour = prompt("Entré le nom");
+let votreprenom = prompt("Prenom ?")
+let adresse = prompt("votre adresse ?")
+alert(name + "\n"+ prenom +"\n"+ ville +"\n"+ bonjour +"\n"+ votreprenom +"\n"+ adresse)
+
